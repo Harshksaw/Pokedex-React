fb3e381da4df4ee3562921078845049509c338b0
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-08-20 23:54:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
+
+export default  defineConfig({
     plugins: [react()],
     base: '/Pokedex-React/',
   }
 
-  if (command !== 'serve') {
-    config.base = '/react-vite-gh-pages/'
-  }
-
-  return config
-})
