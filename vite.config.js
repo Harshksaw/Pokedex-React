75dae82f8f69d3e23b41a736d0ce0e19cdcbd5e9
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-08-20 23:15:29 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base : '/Pokedex-React/'
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/Pokedex-React',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
